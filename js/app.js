2a03fe943f6c3d1744f364ae2a18762bbc9c5671
Pedro de Leon <pedro.deleon@epicor.com>
2025-04-27 18:59:11 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,21 +6,14 @@
 
   //I'm adding this section so I don't have to keep updating this pen every year :-)
   //remove this if you don't need it
-  let today = new Date(),
-    dd = String(today.getDate()).padStart(2, "0"),
-    mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = 2026,
     nextYear = yyyy + 1,
     dayMonth = "04/26/",
-    birthday = dayMonth + yyyy;
+    weddingDay = dayMonth + yyyy;
 
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
-  }
   //end
 
-  const countDown = new Date(birthday).getTime(),
+  const countDown = new Date(weddingDay).getTime(),
     x = setInterval(function() {
 
       const now = new Date().getTime(),
